055a462163f1e9875fb9b9dbf297f56c2692454e
mbussolotto <michele.bussolotto@suse.com>
2022-03-16 17:11:18 +0100

    fix error on jenkins

--- terracumber_config/tf_files/SUSEManager-Test-Hub.tf
+++ terracumber_config/tf_files/SUSEManager-Test-Hub.tf
@@ -1,75 +1,75 @@
 // Mandatory variables for terracumber
 variable "URL_PREFIX" {
-  type = "string"
+  type = string
   default = "https://ci.suse.de/view/Manager/view/Manager-Test/job/manager-TEST-Hub-acceptance-tests"
 }
 
 variable "CUCUMBER_COMMAND" {
-  type = "string"
+  type = string
   default = "export PRODUCT='Uyuni' && run-testsuite #TODO run only sanity, core and hub tests, including reporting"
 }
 
 variable "CUCUMBER_GITREPO" {
-  type = "string"
+  type = string
   default = "https://github.com/uyuni-project/uyuni.git"
 }
 
 variable "CUCUMBER_BRANCH" {
-  type = "string"
+  type = string
   default = "hub-reporting-beta3"
 }
 
 variable "CUCUMBER_RESULTS" {
-  type = "string"
+  type = string
   default = "/root/spacewalk/testsuite"
 }
 
 variable "MAIL_SUBJECT" {
-  type = "string"
+  type = string
   default = "Results TEST-HUB $status: $tests scenarios ($failures failed, $errors errors, $skipped skipped, $passed passed)"
 }
 
 variable "MAIL_TEMPLATE" {
-  type = "string"
+  type = string
   default = "../mail_templates/mail-template-jenkins.txt"
 }
 
 variable "MAIL_SUBJECT_ENV_FAIL" {
-  type = "string"
+  type = string
   default = "Results TEST-HUB : Environment setup failed"
 }
 
 variable "MAIL_TEMPLATE_ENV_FAIL" {
-  type = "string"
+  type = string
   default = "../mail_templates/mail-template-jenkins-env-fail.txt"
 }
 
 variable "MAIL_FROM" {
-  type = "string"
+  type = string
   default = "galaxy-ci@suse.de"
 }
 
 variable "MAIL_TO" {
-  type = "string"
+  type = string
   default = "info@info.de"
 }
 
 // sumaform specific variables
 variable "SCC_USER" {
-  type = "string"
+  type = string
 }
 
 variable "SCC_PASSWORD" {
-  type = "string"
+  type = string
 }
 
 variable "GIT_USER" {
-  type = "string"
+  type = string
   default = null // Not needed for master, as it is public
 }
 
 variable "GIT_PASSWORD" {
-  type = "string"
+  type = string
   default = null // Not needed for master, as it is public
 }
 
@@ -165,9 +165,6 @@ module "min-sles15sp3" {
   provider_settings = {
     mac = "aa:b2:93:01:01:45"
   }
-  server_configuration = {
-    hostname = "suma-testhub-min-sles15sp3.mgr.suse.de"
-  }
 }
 
 module "min-centos7" {
@@ -179,9 +176,6 @@ module "min-centos7" {
   use_os_released_updates = false
   provider_settings = {
     mac = "aa:b2:93:01:01:46"
-  }
-  server_configuration = {
-    hostname = "suma-testhub-min-centos7.mgr.suse.de"
   }
 }
 
@@ -209,10 +203,6 @@ module "controller" {
 
   provider_settings = {
     mac = "aa:b2:93:01:01:41"
-  }
-
-  server_configuration = {
-    hostname = "suma-testhub-ctl.mgr.suse.de"
   }
 }
 
